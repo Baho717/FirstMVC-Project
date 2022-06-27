09989ebff0e98dab5627ab855649eea1c1ba2ddb
Aho, Benjamin <Benjamin.aho27@gmail.com>
2022-06-26 21:18:58 -0400

    files added to be uploaded.

---
+++ JuneMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JuneMVC.Models;
+
+namespace JuneMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static readonly List<DogViewModel> dogs= new List<DogViewModel>(); 
+        public IActionResult Index()
+        {
+
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            // return View();
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        public string Hello()
+        {
+            return "Who's there?";
+        }
+    }
+}
---
+++ JuneMVC/Models/DogViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JuneMVC.Models
+{
+    public class DogViewModel
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
